3daa79aa16b610af46dbc66454c3f6c8b4cb8397
Daniel O'Prey <d@oprey.co.uk>
2015-05-30 16:53:06 -0700

    fire js scroll trigger immediately

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
+    if ($(".navbar").offset().top > 1) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
